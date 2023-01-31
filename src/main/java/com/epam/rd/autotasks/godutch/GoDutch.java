97722164360ca285f47f3cae64966b6500d7fa1a
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-01-31 21:22:38 +0700

    GoDutch_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,25 @@
 package com.epam.rd.autotasks.godutch;
 
+import java.util.Scanner;
 public class GoDutch {
-
     public static void main(String[] args) {
         //Write code here
+        Scanner scan = new Scanner (System.in);
+        int bill = scan.nextInt();
+        if (bill < 0){
+            System.out.println("Bill total amount cannot be negative");
+            return;
+        }
+        int friends = scan.nextInt();
+        if (friends <= 0) {
+            System.out.println("Number of friends cannot be negative or zero");
+            return;
+        }
+
+        int totalSum =bill+bill/10;
+        int part = totalSum/friends;
+
+        System.out.println(part);
+
     }
 }
